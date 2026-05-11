96ef89a7b763e2d3f9ececbadc19fc600fb5379d
Darya Haponava <doe596587@gmail.com>
2026-05-11 23:53:41 +0200

    Preserve target_id in target schema (pair-table)

--- create_data.py
+++ create_data.py
@@ -8,6 +8,8 @@ from rdkit.Chem import MolFromSmiles, Descriptors, rdMolDescriptors
 import networkx as nx
 from utils import *
 
+from kinase_features import build_all_target_feature_tables
+
 # This function one-hot-encodes / numerically encodes the features of a single ligand atom.
 #
 # Original atom features:
@@ -121,13 +123,19 @@ for dataset in datasets:
     ligands = json.load(open(fpath + "ligands_can.txt"), object_pairs_hook=OrderedDict)
     proteins = json.load(open(fpath + "proteins.txt"), object_pairs_hook=OrderedDict)
     affinity = pickle.load(open(fpath + "Y","rb"), encoding='latin1')
+    
     drugs = []
-    prots = []
     for d in ligands.keys():
         lg = Chem.MolToSmiles(Chem.MolFromSmiles(ligands[d]),isomericSmiles=True)
         drugs.append(lg)
+    
+    # to add additional features, we don't drop prot_id 
+    prot_ids = []
+    prot_seqs = []
     for t in proteins.keys():
-        prots.append(proteins[t])
+        prot_ids.append(t)
+        prot_seqs.append(proteins[t])
+    
     if dataset == 'davis':
         affinity = [-np.log10(y/1e9) for y in affinity]
     affinity = np.asarray(affinity)
@@ -139,18 +147,19 @@ for dataset in datasets:
         elif opt=='test':
             rows,cols = rows[valid_fold], cols[valid_fold]
         with open('data/' + dataset + '_' + opt + '.csv', 'w') as f:
-            f.write('compound_iso_smiles,target_sequence,affinity\n')
+            f.write('compound_iso_smiles,target_id,target_sequence,affinity\n')
             for pair_ind in range(len(rows)):
                 ls = []
                 ls += [ drugs[rows[pair_ind]]  ]
-                ls += [ prots[cols[pair_ind]]  ]
+                ls += [ prot_ids[cols[pair_ind]] ]
+                ls += [ prot_seqs[cols[pair_ind]] ]
                 ls += [ affinity[rows[pair_ind],cols[pair_ind]]  ]
                 f.write(','.join(map(str,ls)) + '\n')       
     print('\ndataset:', dataset)
     print('train_fold:', len(train_fold))
     print('test_fold:', len(valid_fold))
-    print('len(set(drugs)),len(set(prots)):', len(set(drugs)),len(set(prots)))
-    all_prots += list(set(prots))
+    print('len(set(drugs)),len(set(prots)):', len(set(drugs)),len(set(prot_ids)))
+    all_prots += list(set(prot_seqs))
 
 # This next part:
 # protein sequence letters → integer numbers
@@ -160,6 +169,9 @@ seq_voc = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
 seq_dict = {v:(i+1) for i,v in enumerate(seq_voc)}
 seq_dict_len = len(seq_dict)
 max_seq_len = 1000
+
+# trigger protein-feature table build
+build_all_target_feature_tables()
 
 compound_iso_smiles = []
 for dt_name in ['kiba','davis']:
